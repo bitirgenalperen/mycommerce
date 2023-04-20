88e8e9f43c84f8352e857bfc79af23c55c2d4166
Alperen <bitirgenalperen@gmail.com>
2023-04-20 15:12:47 +0300

    first setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>HELLO</h1>
+    </div>
+  );
+}
+
+export default App;
