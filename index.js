2cc9407a4fc07deb0bc7481b19a7dc2497961841
Alperen <bitirgenalperen@gmail.com>
2023-04-23 18:00:39 +0300

    update style

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const methodOverride = require('method-override')
 const morgan = require("morgan");
 const ejsMate = require('ejs-mate');
+const ErrorHandler = require('./utils/ErrorHandler');
+const AsyncWrapper = require('./utils/AsyncWrapper');
 
 dotenv.config();
 const app = express();
@@ -42,7 +44,7 @@ app.get('/', (req, res) => {
     res.redirect('/items');
 })
 
-app.get('/items', async (req, res) => {
+app.get('/items', AsyncWrapper(async (req, res) => {
     let {article} = req.query;
     if(article){
         const items = await Item.find({article});
@@ -53,49 +55,49 @@ app.get('/items', async (req, res) => {
         res.render('items/index', {items, article: "All"});
     }
     
-})
+}))
 
 app.get("/items/new", (req, res) => {
     res.render('items/new');
     
 })
 
-app.post('/items', async (req, res) => {
+app.post('/items', AsyncWrapper(async (req, res) => {
     const {name, description, article, price, image} = req.body;
     // console.log(name, description, article, price, image);
     const newItem = new Item({name, description, article, price, image});
     await newItem.save();
     res.redirect(`/items/${newItem._id}/edit`);
-})
+}))
 
-app.get('/items/:id', async (req, res) => {
+app.get('/items/:id', AsyncWrapper(async (req, res) => {
     const {id} = req.params;
     const item = await Item.findById(id).populate('reviews');
     res.render('items/show', {item});
-})
+}))
 
-app.get('/items/:id/edit', async (req, res) => {
+app.get('/items/:id/edit', AsyncWrapper(async (req, res) => {
     const {id} = req.params;
     const item = await Item.findById(id);
     res.render('items/edit', {item});
-})
+}))
 
 // category değişimine uyum saglayacak degisiklikleri yap!
-app.put('/items/:id', async (req, res) => {
+app.put('/items/:id', AsyncWrapper(async (req, res) => {
     const {id} = req.params;
     const item = await Item.findByIdAndUpdate(id, req.body, {runValidators:true});
     res.redirect(`/items/${item._id}`);
-})
+}))
 
-app.delete('/items/:id', async (req, res) => {
+app.delete('/items/:id', AsyncWrapper(async (req, res) => {
     const {id} = req.params;
     await Item.findByIdAndDelete(id);
     res.redirect('/items');
-})
+}))
 
 // catch async function ekle
 // add a review
-app.post('/items/:id/reviews', async (req, res) => {
+app.post('/items/:id/reviews', AsyncWrapper(async (req, res) => {
     const {id} = req.params;
     const item = await Item.findById(id);
     const {content, rating} = req.body;
@@ -104,31 +106,36 @@ app.post('/items/:id/reviews', async (req, res) => {
     await review.save();
     await item.save();
     res.redirect(`/items/${id}`);
-})
+}))
 
-app.get('/items/:itemId/reviews/:reviewId/edit', async (req, res) => {
+app.get('/items/:itemId/reviews/:reviewId/edit', AsyncWrapper(async (req, res) => {
     const {itemId, reviewId} = req.params;
     const review = await Review.findById(reviewId );
     res.render('reviews/edit', {review, itemId});
-})
+}))
 
-app.put('/items/:itemId/reviews/:reviewId', async (req, res) => {
+app.put('/items/:itemId/reviews/:reviewId', AsyncWrapper(async (req, res) => {
     const {itemId, reviewId} = req.params;
     const review = await Review.findByIdAndUpdate(reviewId, req.body, {runValidators:true});
     res.redirect(`/items/${itemId}`);
-})
+}))
 
 // delete the review
-app.delete('/items/:itemId/reviews/:reviewId', async (req, res) => {
+app.delete('/items/:itemId/reviews/:reviewId', AsyncWrapper(async (req, res) => {
     const {itemId, reviewId} = req.params;
     await Item.findByIdAndUpdate(itemId, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
     res.redirect(`/items/${itemId}`);
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ErrorHandler('Page Couldnot Be Reached!', 404));
 })
 
 // error handling mesajlarını guncelle => yeni bir sayfa olustur
-app.use((req, res) => {
-    res.status(404).send("NOT FOUND");
+app.use((err, req, res, next) => {
+    const {message = "Invalid Request", status = 405} = err;
+    res.status(status).render('utils/error', {status, message});
 })
 
 
