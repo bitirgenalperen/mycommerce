3b2da347b1199c2dcc452e8ceac37979d343e7b9
Alperen <bitirgenalperen@gmail.com>
2023-04-24 21:14:43 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ router.post('/register', AsyncWrapper(async(req, res, next) => {
     try {
         const {email, username, password} = req.body;
         const newUser = new User({email, username})
-        res.redirect('/items');
+        res.redirect('/users');
     } catch (error) {
         res.redirect('/register');
     }
